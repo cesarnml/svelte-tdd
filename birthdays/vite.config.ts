3bbd807c8cfb7b9ec19fc5aa600fb7f9ae211191
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-11-03 10:05:49 +0700

    complete chaper 01

--- birthdays/vite.config.ts
+++ birthdays/vite.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-		// reporters: ['verbose']
+		globals: true,
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		setupFiles: ['src/vitest/registerMatchers.ts', 'src/vitest/cleanupDom.ts'],
+		restoreMocks: true
 	}
 });
